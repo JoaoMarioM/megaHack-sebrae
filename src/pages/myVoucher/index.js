c90c739ce391ebbaed80cd5b8425243fc5f0d68d
Joao Mario <joao.mariojm7@hotmail.com>
2020-07-05 22:14:03 -0300

    profile

--- src/pages/myVoucher/index.js
+++ src/pages/myVoucher/index.js
@@ -2,6 +2,13 @@ import React from 'react';
 import { View } from 'react-native';
 
 export default function MyVoucher() {
+
+  const navigation = useNavigation()
+
+  // function ToVoucher(){
+  //     navigation.navigate('Restaurant')
+  // }
+
  return (
    <View/>
   );
